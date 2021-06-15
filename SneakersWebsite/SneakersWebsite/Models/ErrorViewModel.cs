16ba10e52fa0f4269e2a138b0862536d6ee61cfc
Orlando <53872517+orlando1424@users.noreply.github.com>
2021-06-15 09:09:36 -0600

    init
    initial commit

---
+++ SneakersWebsite/SneakersWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SneakersWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
