b91dea1456fd024d3a189162512f7a815d0e07e6
Orlando <53872517+orlando1424@users.noreply.github.com>
2021-06-25 10:51:10 -0600

    second shoe page added
    shoe page added for the nike blazers

--- SneakersWebsite/SneakersWebsite/Controllers/ShoePagesController.cs
+++ SneakersWebsite/SneakersWebsite/Controllers/ShoePagesController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,6 +14,11 @@ namespace SneakersWebsite.Controllers
 
 
         public IActionResult AdidasHu()
+        {
+            return View();
+        }
+
+        public IActionResult BlazerMidSacai()
         {
             return View();
         }
